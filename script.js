af274547d02b1ff04dce38b1cd0a198717d492be
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-03-26 18:17:18 +0300

    linking

---
+++ script.js
@@ -0,0 +1,9 @@
+function onSubmit() {
+    alert("Form submitted successfully");
+    return true;
+    }
+    
+    function welcomeMsg(name) {
+        return ("Hello " + name + ", welcome to our website!")
+    }
+    
